3fe10e0ad63fb55429697589afbcebc914faa7f3
Jayavardhan-J <Jayavardhanjj3184@gmail.com>
2021-10-19 19:04:38 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,66 @@
+let globalTaskData = [];
+taskContents = document.getElementById("taskContentsrow");
+
+const addCard = () => {
+    const newTaskDetails = {
+        id: `${Date.now()}`,
+        url: document.getElementById("imageURL").value,
+        title: document.getElementById("taskTitle").value,
+        type: document.getElementById("taskType").value,
+        description: document.getElementById("taskDescription").value
+    };
+
+    taskContents.insertAdjacentHTML('beforeend', generateTaskCard(newTaskDetails));
+
+    globalTaskData.push(newTaskDetails);
+    saveToLocalStorage();
+}
+
+const generateTaskCard = ({id, url, title, type, description}) => {
+    return (`<div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
+        <div class="card card__color">
+            <div class="card-header">
+                <div class="d-flex justify-content-end">
+                    <button type="button" class="btn btn-info card__button ">
+                        <i class="fas fa-pencil-alt"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger card__button" name=${id} onclick="deleteTask(this)">
+                        <i class="far fa-trash-alt" name=${id} onclick="deleteTask(this)"></i>
+                    </button>
+                </div>
+            </div>
+            <img src=${url} class="card-img-top" alt="image"/>
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${description}</p>
+                <span class="badge bg-primary">${type}</span>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary float-end">OPEN TASK</button>
+            </div>
+        </div>
+    </div>`)
+}
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("nikhiltasks", JSON.stringify({nikhil: globalTaskData}));
+}
+
+const reloadTaskCard = () => {
+    const localStorageCopy = JSON.parse(localStorage.getItem("nikhiltasks"));
+    console.log(localStorageCopy);
+    if(localStorageCopy) {
+        globalTaskData = localStorageCopy["nikhil"];
+    }
+    console.log(globalTaskData)
+    globalTaskData.map((cardData) => {
+        taskContents.insertAdjacentHTML('beforeend', generateTaskCard(cardData));
+    })
+}
+
+const deleteTask = (e) => {
+    const targetID = e.getAttribute("name");
+    globalTaskData = globalTaskData.filter((cardData) => cardData.id!==targetID);
+    saveToLocalStorage();
+    window.location.reload();
+}
